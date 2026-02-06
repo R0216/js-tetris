3ab8acc8b7fc7a2be87977837efd33c3f67a5302
R0216 <s1f102500247@iniad.org>
2026-02-06 12:38:11 +0900

    自動落下関数の制作

--- script.js
+++ script.js
@@ -3,11 +3,36 @@ const canvas = document.getElementById("tetris");
 const context = canvas.getContext("2d");
 
 context.scale(20, 20);
-context.fillStyle = "red";
-context.fillRect(2, 2, 1, 1);
 
+const player = {
+    pos: {x: 5, y: 2},
+    matrix: PIECES['T'],
+};
+drawMatrix(player.matrix, player.pos);
 
+let dropCounter = 0;
+let dropInterval = 1000;
+let lastTime = 0;
 
+function draw() {
+    context.fillStyle = '#000';
+    context.fillRect(0, 0, canvas.clientWidth, canvas.height);
+    context.fillStyle = 'red';
+    drawMatrix(player.matrix, player.pos);
+}
+
+function update(time=0){
+    const deltaTime = time - lastTime;
+    lastTime = time;
+
+    dropCounter += deltaTime;
+    if (dropCounter > dropInterval){
+        player.pos.y++;
+        dropCounter = 0;
+    }
+    draw();
+    requestAnimationFrame(update);
+}
 function drawMatrix(matrix, offset) {
     matrix.forEach((row, y) => {
         row.forEach((value, x) => {
@@ -18,8 +43,4 @@ function drawMatrix(matrix, offset) {
     });
 }
 
-const player = {
-    pos: {x: 5, y: 2},
-    matrix: PIECES['T'],
-};
-drawMatrix(player.matrix, player.pos);
+update();
